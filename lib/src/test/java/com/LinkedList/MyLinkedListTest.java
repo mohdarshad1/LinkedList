dd2e1d4428c7e10e1dbdd7aaafd251ac2f198b5d
mohdarshad1 <mdarshad181@gmail.com>
2021-05-05 22:21:47 +0530

    [Mohd Arshad] : Add Adding Node

---
+++ lib/src/test/java/com/LinkedList/MyLinkedListTest.java
@@ -0,0 +1,23 @@
+package com.LinkedList;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class MyLinkedListTest {
+
+	@Test
+	public void given3Numbers_WhenAdded_ShouldBeAddedToTop() {
+		MyNode<Integer> myFirstNode = new MyNode<>(70);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.add(mySecondNode);
+		myLinkedList.add(myThirdNode);
+		myLinkedList.printMyNode();
+		boolean result = myLinkedList.head.equals(myThirdNode) && 
+						 myLinkedList.head.getNext().equals(mySecondNode) && 
+						 myLinkedList.tail.equals(myFirstNode);
+		assertTrue(result);
+	}
+}
